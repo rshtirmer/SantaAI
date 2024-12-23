b011a27b3c487dc17ddfd51d444a7e6e715f4a57
shira eisenberg <shiraeisenberg@uchicago.edu>
2024-12-23 16:28:45 -0500

    prompt updates

--- santa-letter-t3/.next/server/pages/_document.js
+++
@@ -1,5 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/pages/_document.js";
-const runtime = require("../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/node_modules__pnpm_e4570f._.js");
-runtime.loadChunk("server/chunks/ssr/[root of the server]__e237d7._.js");
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/.pnpm/next@15.1.2_react-dom@18.3.1_react@18.3.1__react@18.3.1/node_modules/next/document.js [ssr] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
